97ffadce8463f27a9eea4a6d3a20b17665158d81
Pate1337 <paavo.hemmo@hotmail.com>
2018-12-02 15:36:47 +0200

    Deleted all the demo stuff

--- application/views.py
+++ application/views.py
@@ -1,24 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from application import app
-
-class Item:
-    def __init__(self, name):
-        self.name = name
-
-nimi = "Essi Esimerkki"
-
-lista = [1, 1, 2, 3, 5, 8, 11]
-
-esineet = []
-esineet.append(Item("Eka"))
-esineet.append(Item("Toka"))
-esineet.append(Item("Kolmas"))
-esineet.append(Item("Neljäs"))
   
 @app.route("/")
 def hello():
-    return render_template("index.html")
-
-@app.route("/demo")
-def content():
-    return render_template("demo.html", nimi=nimi, lista=lista, esineet=esineet)
+    return redirect(url_for("categories_index"))
