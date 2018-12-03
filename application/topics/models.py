560b7c0f4cd99211f3e7bf3247666805b6ee8144
Pate1337 <paavo.hemmo@hotmail.com>
2018-12-03 10:33:04 +0200

    Heroku hajosi sql kyselyn seurauksena. Koitetaas jos tällä toimis.

--- application/topics/models.py
+++ application/topics/models.py
@@ -16,12 +16,17 @@ class Topic(Base):
     
     @staticmethod
     def find_topics_for_category_with_users(category_id):
-        # This would be good for finding the latest by date_modified
-        stmt = text("SELECT topic.name, topic.id AS topic_id, account.username, account.id AS account_id, messages.date_created AS latest FROM topic, account"
-                     " LEFT JOIN (SELECT * FROM message ORDER BY date_created ASC) AS messages"
+        stmt = text("SELECT topic.name, topic.id AS topic_id, account.username, account.id AS account_id, messages.maxDate AS latest FROM topic, account"
+                     " LEFT JOIN (SELECT topic_id, max(date_created) AS maxDate FROM message GROUP BY topic_id) messages"
                      " ON messages.topic_id = topic.id"
                      " WHERE category_id = :category_id AND topic.account_id = account.id"
-                     " GROUP BY topic.id ORDER BY latest DESC").params(category_id = category_id)
+                     " ORDER BY latest DESC").params(category_id = category_id)
+        # This would be good for finding the latest by date_modified
+        #stmt = text("SELECT topic.name, topic.id AS topic_id, account.username, account.id AS account_id, messages.date_created AS latest FROM topic, account"
+        #             " LEFT JOIN (SELECT * FROM message ORDER BY date_created ASC) messages"
+        #             " ON messages.topic_id = topic.id"
+        #             " WHERE category_id = :category_id AND topic.account_id = account.id"
+        #             " GROUP BY topic.id ORDER BY latest DESC").params(category_id = category_id)
 
         # This relies heavily on GROUP BY returning the last record...Dunno why, but it does
         #stmt = text("SELECT topic.name, topic.id AS topic_id, account.username, account.id AS account_id, message.date_created AS latest FROM topic, account"
@@ -38,3 +43,15 @@ class Topic(Base):
             response.append({"topic":{ "name": row[0], "id": row[1], "latest_message": latest_message }, "account": { "username": row[2], "id": row[3] }})
 
         return response
+
+# SELECT topic.name, topic.id AS topic_id, account.username, account.id AS account_id, messages.date_created AS latest FROM topic, account LEFT JOIN (SELECT * FROM message ORDER BY date_created ASC) messages ON messages.topic_id = topic.id WHERE category_id = 2 AND topic.account_id = account.id GROUP BY topic.id ORDER BY latest DESC
+
+# select t.username, t.date, t.value
+#from MyTable t
+#inner join (
+#    select username, max(date) as MaxDate
+#    from MyTable
+#    group by username
+#) tm on t.username = tm.username and t.date = tm.MaxDate
+
+# SELECT topic.name, topic.id AS topic_id, account.username, account.id AS account_id, messages.maxDate AS latest FROM topic, account LEFT JOIN (SELECT topic_id, max(date_created) AS maxDate FROM message GROUP BY topic_id) messages ON messages.topic_id = topic.id WHERE category_id = 2 AND topic.account_id = account.id ORDER BY latest DESC
