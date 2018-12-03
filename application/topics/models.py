37e067dba3d79ce3bf3803a2e7ae93314f867242
Pate1337 <paavo.hemmo@hotmail.com>
2018-12-03 10:48:33 +0200

    Vieläkään ei toimi...

--- application/topics/models.py
+++ application/topics/models.py
@@ -16,10 +16,10 @@ class Topic(Base):
     
     @staticmethod
     def find_topics_for_category_with_users(category_id):
-        stmt = text("SELECT topic.name, topic.id, account.username, account.id AS account_id, messages.maxDate AS latest FROM topic, account"
+        stmt = text("SELECT t.name, t.id, account.username, account.id AS account_id, messages.maxDate AS latest FROM topic t, account"
                      " LEFT JOIN (SELECT topic_id, max(date_created) AS maxDate FROM message GROUP BY topic_id) messages"
-                     " ON messages.topic_id = topic.id"
-                     " WHERE category_id = :category_id AND topic.account_id = account.id"
+                     " ON messages.topic_id = t.id"
+                     " WHERE category_id = :category_id AND t.account_id = account.id"
                      " ORDER BY latest DESC").params(category_id = category_id)
         # This would be good for finding the latest by date_modified
         #stmt = text("SELECT topic.name, topic.id AS topic_id, account.username, account.id AS account_id, messages.date_created AS latest FROM topic, account"
@@ -54,4 +54,4 @@ class Topic(Base):
 #    group by username
 #) tm on t.username = tm.username and t.date = tm.MaxDate
 
-# SELECT topic.name, topic.id, account.username, account.id AS account_id, messages.maxDate AS latest FROM topic, account LEFT JOIN (SELECT topic_id, max(date_created) AS maxDate FROM message GROUP BY topic_id) messages ON messages.topic_id = topic.id WHERE category_id = 2 AND topic.account_id = account.id ORDER BY latest DESC
+# SELECT t.name, t.id, account.username, account.id AS account_id, messages.maxDate AS latest FROM topic t, account LEFT JOIN (SELECT topic_id, max(date_created) AS maxDate FROM message GROUP BY topic_id) messages ON messages.topic_id = t.id WHERE category_id = 2 AND t.account_id = account.id ORDER BY latest DESC
