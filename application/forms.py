c4b503aa80336a762e40a9bb96a9a2e68f3e6fee
Pate1337 <paavo.hemmo@hotmail.com>
2018-12-16 20:08:31 +0200

    Poistettu turha forms tiedosto

--- application/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, validators
-
-class SearchForm(FlaskForm):
-    query = StringField("Search", [validators.Length(min=1, message="Kentässä on oltava vähintään 1 merkki"), validators.Length(max=20)])
- 
-    class Meta:
-        csrf = False
